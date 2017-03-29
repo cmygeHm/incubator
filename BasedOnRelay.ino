418b01ff82a63f79c9fb987f488189a817b254e4
Артур Гафаров <a.gafarov@elama.ru>
2017-03-29 15:02:20 +0300

    Update BasedOnRelay.ino
    Отказался от Thread и Controller'a. Смысла нет, если "поток" оказался блокирующим. Плюс так будет легче рядовому птицеводу смастерить инкубатор.

--- BasedOnRelay.ino
+++ BasedOnRelay.ino
@@ -3,8 +3,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <DHT.h>
-#include <Thread.h>
-#include <ThreadController.h>
 
 // Set the LCD address to 0x27 for a 16 chars and 2 l+66ine display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -21,44 +19,40 @@ DHT dht(3, DHT22);
 
 #define H_RELEY_PIN 4
 #define T_RELEY_PIN 5
-#define SERVO_PIN 12
-#define AERATION_PIN 6
 
-Thread * servoThread = new Thread();
-Thread * aerationThread = new Thread();
-ThreadController controller = ThreadController();
+#define SERVO_PIN 12
+#define SERVO_INTERVAL 3600000; // 60 * 60 * 1 * 1000 = один час
+
+#define AERATION_PIN 6
+#define AERATION_INTERVAL 10800000 // 60*60*3*1000 = 3 часа
+
+unsigned long aerationStartTime = 0;
+unsigned long aerationFinishTime = 0;
+
+unsigned long servoSwitchTime = 0;
+bool servoState = false;
 
 void setup()
 {
-  Serial.begin(9600);
   lcd.begin();
   sensor.begin();
   dht.begin();
 
-	// Turn on the blacklight and print a message.
-	lcd.backlight();
+  aerationStartTime = millis() + AERATION_INTERVAL;
+  servoSwitchTime = millis() + SERVO_INTERVAL;
+
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
   pinMode(H_RELEY_PIN, OUTPUT);
   pinMode(T_RELEY_PIN, OUTPUT);
   pinMode(SERVO_PIN, OUTPUT);
   pinMode(AERATION_PIN, OUTPUT);
-  
-  servoThread->onRun(switchServo);
-  servoThread->setInterval(3600000); // 60 * 60 * 1 * 1000
-
-  aerationThread->onRun(switchAeration);
-  aerationThread->setInterval(10800000); // 60 * 60 * 3 * 1000
-
-  controller.add(servoThread);
-  controller.add(aerationThread);
 }
 
 void loop()
 {
-  Serial.begin(9600);
-  controller.run();
-  
   lcd.clear();
-	sensor.requestTemperatures();
+  sensor.requestTemperatures();
   float t = sensor.getTempCByIndex(0);
   lcd.print("t:");
   lcd.print(t);
@@ -88,24 +82,34 @@ void loop()
     } else {
       digitalWrite(H_RELEY_PIN, true);
     }
-  }  
+  }
 
-  delay(2000);
+  switchAeration();
+  switchServo();
+  
+  delay(1000);
 }
 
 void switchServo()
 {
-  static bool servoState = true;
-  digitalWrite(SERVO_PIN, servoState);
-  servoState = !servoState;
+  unsigned long currentMillis = millis();
+  if (servoSwitchTime < currentMillis) {
+    servoSwitchTime = currentMillis + SERVO_INTERVAL;
+    servoState = !servoState;
+    digitalWrite(SERVO_PIN, servoState);
+  }
 }
 
 void switchAeration()
-{
-  digitalWrite(AERATION_PIN, false);
-  unsigned long loopTime = millis();
-  while (millis() < (loopTime + 180000)) { // 60 * 3 * 1000
-    delay(1000);
+{  
+  unsigned long currentMillis = millis();
+  if (aerationStartTime < currentMillis) {
+    aerationFinishTime = currentMillis + 180000; // 60*3*1000 = 3 минуты
+    aerationStartTime = aerationFinishTime + AERATION_INTERVAL;
+    digitalWrite(AERATION_PIN, false);
   }
-  digitalWrite(AERATION_PIN, true);
+
+  if (aerationFinishTime < currentMillis) {
+    digitalWrite(AERATION_PIN, true);
+  }
 }
